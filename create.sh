b8812a58d2bb8415c98dc1cc7cc27ff023323af1
virtustate <stevek@virtustate.com>
2013-02-17 17:46:22 -0600

    Update create.sh

--- create.sh
+++ create.sh
@@ -6,4 +6,4 @@ drush make d7standard.distro.make $installdir
 cp $installdir/sites/default/default.settings.php $installdir/sites/default/settings.php
 chown -R $permschema $installdir/sites/
 chmod -R g+w $installdir/sites/
-mysql -u < create.sql
+mysql < create.sql
